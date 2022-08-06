fe448e56f787bea87840e75af5ed19126260eb19
Priscila <cyllac@gmail.com>
2022-08-06 17:28:36 -0300

    Primeiro commit. Demonstrativo de Activities.

---
+++ app/src/main/java/com/cursoandroid/priscila/cyllacempresaconsultoria/EmpresaActivity.java
@@ -0,0 +1,13 @@
+package com.cursoandroid.priscila.cyllacempresaconsultoria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EmpresaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa);
+    }
+}
---
+++ app/src/main/java/com/cursoandroid/priscila/cyllacempresaconsultoria/ServicoActivity.java
@@ -0,0 +1,13 @@
+package com.cursoandroid.priscila.cyllacempresaconsultoria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ServicoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servico);
+    }
+}
